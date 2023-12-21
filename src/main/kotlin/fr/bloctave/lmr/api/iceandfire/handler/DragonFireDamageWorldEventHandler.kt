1a78d4b31810740eb1adebfb773de8d4aab9fd95
0ctave <octave.boelle@hotmail.fr>
2023-12-21 14:41:58 +0100

    Ver. 2.0.3 Fixes on gradle settings

--- src/main/kotlin/fr/bloctave/lmr/api/iceandfire/handler/DragonFireDamageWorldEventHandler.kt
+++ src/main/kotlin/fr/bloctave/lmr/api/iceandfire/handler/DragonFireDamageWorldEventHandler.kt
@@ -4,7 +4,6 @@ import com.github.alexthe666.iceandfire.api.event.DragonFireDamageWorldEvent
 import fr.bloctave.lmr.api.iceandfire.IceAndFireConfig
 import fr.bloctave.lmr.api.proxy.IEventHandler
 import fr.bloctave.lmr.util.EventUtil
-import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 import net.minecraftforge.eventbus.api.SubscribeEvent
@@ -16,14 +15,14 @@ class DragonFireDamageWorldEventHandler :
     override fun handleEvent(event: DragonFireDamageWorldEvent) = event.run {
         val area = EventUtil.basicChecks(entity, entity.level as World, BlockPos(targetX, targetY, targetZ)) ?: return@run
 
-        val pos = BlockPos(targetX, targetY, targetZ)
+        /*val pos = BlockPos(targetX, targetY, targetZ)
         val level = dragon.level
         val stage = dragon.dragonStage
         val j = 2
         val k = 2
         val l = 2
 
-        val player: PlayerEntity? = dragon.ridingPlayer
+        val player: PlayerEntity? = dragon.ridingPlayer*/
 
         /*if (stage <= 3) {
             BlockPos.betweenClosedStream(pos.offset(-j, -k, -l), pos.offset(j, k, l)).forEach { pos ->
